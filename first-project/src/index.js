295997e7b7a6454cffe3af3c9107493b96bebc96
JlordS32 <jlordsty128@gmail.com>
2023-04-23 02:14:13 +1000

    initial commit

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -1,16 +1,21 @@
 // index.js
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import asabenehImage from './images/asabeneh.jpg';
+import cssLogo from './images/css_logo.png';
+import htmlLogo from './images/html_logo.png';
+import reactLogo from './images/react_logo.png';
 
 // JSX element, header
-const welcome = 'Welcome to 30 Days Of React'
-const title = 'Getting Started React'
-const subtitle = 'JavaScript Library'
+const welcome = 'Welcome to 30 Days Of React';
+const title = 'Getting Started React';
+const subtitle = 'JavaScript Library';
 const author = {
   firstName: 'Asabeneh',
   lastName: 'Yetayeh',
 }
-const date = 'Oct 2, 2020'
+const date = 'Oct 2, 2020';
+const copyRight = 'Copyright 2020';
 
 // JSX element, header
 const header = (
@@ -25,16 +30,7 @@ const header = (
       <small>Date: {date}</small>
     </div>
   </header>
-)
-
-const numOne = 3
-const numTwo = 2
-
-const result = (
-  <p>
-    {numOne} + {numTwo} = {numOne + numTwo}
-  </p>
-)
+);
 
 const yearBorn = 1820
 const currentYear = new Date().getFullYear()
@@ -44,50 +40,62 @@ const personAge = (
     {' '}
     {author.firstName} {author.lastName} is {age} years old
   </p>
-)
+);
 
 // JSX element, main
 const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>)
+const techsFormatted = techs.map((tech) => <li>{tech}</li>);
+
+const user = (
+  <div>
+    <img src={asabenehImage} alt='asabeneh image' />
+  </div>
+)
 
 // JSX element, main
 const main = (
   <main>
     <div className='main-wrapper'>
-      <p>
-        Prerequisite to get started{' '}
-        <strong>
-          <em>react.js</em>
-        </strong>
-        :
-      </p>
-      <ul>{techsFormatted}</ul>
-      {result}
-      {personAge}
     </div>
   </main>
+);
+
+// following images
+const frontEndTech = (
+  <div class='frontEnd-container'>
+    <h2><strong>Front End Technologies</strong></h2>
+    <div className='frontEndImg'>
+      <img src={cssLogo} alt='CSS Logo'/>
+      <img src={htmlLogo} alt='HTML Logo'/>
+      <img src={reactLogo} alt='React Logo'/>
+    </div>
+  </div>
 )
 
-const copyRight = 'Copyright 2020'
-
+const body = (
+  <div>
+    {frontEndTech}
+  </div>
+)
 // JSX element, footer
 const footer = (
   <footer>
     <div className='footer-wrapper'>
-      <p>{copyRight}</p>
+      <p>&copy; {copyRight}</p>
     </div>
   </footer>
-)
+);
 
 // JSX element, app
 const app = (
   <div className='app'>
     {header}
     {main}
+    {body}
     {footer}
   </div>
-)
+);
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById('root');
 // we render the JSX element using the ReactDOM package
-ReactDOM.render(app, rootElement)
+ReactDOM.render(app, rootElement);
