20d6ea36489fb7252bfb867e0adb78dff89007c2
JlordS32 <jlordsty128@gmail.com>
2023-04-23 14:56:05 +1000

    new update

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -1,18 +1,20 @@
 // index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
-import asabenehImage from './images/asabeneh.jpg';
+import personImage from './images/jaylou.jpg';
 import cssLogo from './images/css_logo.png';
 import htmlLogo from './images/html_logo.png';
 import reactLogo from './images/react_logo.png';
+import checkIcon from './images/checkicon.svg';
+
 
 // JSX element, header
 const welcome = 'Welcome to 30 Days Of React';
 const title = 'Getting Started React';
 const subtitle = 'JavaScript Library';
 const author = {
-  firstName: 'Asabeneh',
-  lastName: 'Yetayeh',
+  firstName: 'Jaylou',
+  lastName: 'Rasonabe',
 }
 const date = 'Oct 2, 2020';
 const copyRight = 'Copyright 2020';
@@ -32,26 +34,6 @@ const header = (
   </header>
 );
 
-const yearBorn = 1820
-const currentYear = new Date().getFullYear()
-const age = currentYear - yearBorn
-const personAge = (
-  <p>
-    {' '}
-    {author.firstName} {author.lastName} is {age} years old
-  </p>
-);
-
-// JSX element, main
-const techs = ['HTML', 'CSS', 'JavaScript']
-const techsFormatted = techs.map((tech) => <li>{tech}</li>);
-
-const user = (
-  <div>
-    <img src={asabenehImage} alt='asabeneh image' />
-  </div>
-)
-
 // JSX element, main
 const main = (
   <main>
@@ -60,7 +42,7 @@ const main = (
   </main>
 );
 
-// following images
+// Front end tech section
 const frontEndTech = (
   <div class='frontEnd-container'>
     <h2><strong>Front End Technologies</strong></h2>
@@ -72,11 +54,55 @@ const frontEndTech = (
   </div>
 )
 
-const body = (
-  <div>
-    {frontEndTech}
+// Subscription box
+const subscriptionbox = (
+    <div className='subscription-box'>
+        <h1>Subscribe</h1>
+        <p>Sign up with your email address to receive news and letters!</p>
+        <div className='textbox-container'>
+          <input type='text' placeholder='First Name'></input>
+          <input type='text' placeholder='Last Name'></input>
+          <input type='text' placeholder='Email'></input>
+        </div>
+        <button>Subscribe</button>
+    </div>
+)
+
+const userCard = (
+  <div class='userCard'>
+    <div class='userCard-container'>
+      <div className='author-details'>
+        <img src={personImage} alt='Author Image'/>
+        <div className='person-name'>
+          <h2>{author.firstName} {author.lastName}</h2>
+          <img src={checkIcon} alt='Check Icon'/>
+        </div>
+        <h3>Senior Developer, Finland</h3>
+      </div>
+      <div className='skills-container'>
+        <h2>Skills</h2>
+        <div className='skillsTag-container'>
+          <div>HTML</div>
+          <div>Sass</div>
+        </div>
+      </div>
+      <div className='joined-date'>
+        <p></p>
+      </div>
+    </div>
   </div>
 )
+
+const body = (
+  <div>
+    <div className='body-container'>
+      {userCard}
+      {frontEndTech}
+      {subscriptionbox}
+    </div>
+  </div>
+)
+
 // JSX element, footer
 const footer = (
   <footer>
