3e5ba264cb254680990b9a5be2654b9f2ca0f103
JlordS32 <jlordsty128@gmail.com>
2023-04-24 21:11:25 +1000

    finished number generator

--- number-generator/src/index.js
+++ number-generator/src/index.js
@@ -7,29 +7,69 @@ const color = {
   green: '#21bf73'
 }
 
+const primeColor = {
+  backgroundColor: color.red
+}
+
+function isPrime(num) {
+  if (num <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
 const Header = ({data: {
   title,
   subtitle
 }}) => {
   return (
-    <header>
+    <header class='header-container'>
       <h1>{title}</h1>
       <h2>{subtitle}</h2>
     </header>
   )
 }
 
+const Number = () => {
+  const boxes = [];
+  
+  for (let i = 0; i <= 31; i++) {
+    if (isPrime(i)) {
+      boxes.push(<div key={i} style={primeColor}className='numbered-box'><span>{i}</span></div>);
+    }
+
+    else {
+      boxes.push(<div key={i} style={{backgroundColor: i % 2 === 0 ? color.green : color.yellow}}className='numbered-box'><span>{i}</span></div>);
+    }
+  }
+
+  return <div>{boxes}</div>;
+};
+
+const Boxes = () => {
+  return (
+    <div className='box-container'>
+      <Number />
+    </div>
+  )
+}
+
+
 const App = () => {
 
   const data = {
-  title: '30 Days of React',
+  title: '30 Days Of React',
   subtitle: 'Number Generator'
   }
 
   return (
-    <div className='container'>
-      <div>
+    <div className='app'>
+      <div className='container'>
         <Header data={data}/>
+        <Boxes />
       </div>
     </div>
   )
