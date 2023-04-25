574af7191d1314dc804983cd9379de73185e8ccc
JlordS32 <jlordsty128@gmail.com>
2023-04-25 22:01:55 +1000

    done

--- learning-react-4/src/index.js
+++ learning-react-4/src/index.js
@@ -1,20 +1,175 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import personImage from './images/asabeneh.jpg';
+
+const UserCard = ({user: {firstName, lastName, image}}) => (
+  <div className='user-card'>
+    <img src={image} alt={firstName}/>
+    <h2>
+      {firstName}
+      {lastName}
+    </h2>
+  </div>
+)
+
+const Button = ({ text, onClick, style }) => (
+  <button style={style} onClick={onClick}>
+    {text} 
+  </button>
+)
+
+const buttonStyles = {
+  backgroundColor: '#61dbfb',
+  padding: 10,
+  border: 'none',
+  borderRadius: 5,
+  margin: 3,
+  cursor: 'pointer',
+  fontSize: 18,
+  color: 'white',
+}
 
 // class based component
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
     return (
       <header>
-        <h1>Welcome to 30 Days Of React</h1>
-        <h2>Getting Started React</h2>
-        <h3>JavaScript Library</h3>
-        <p>Asabeneh Yetayeh</p>
-        <small>Oct 6, 2020</small>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>{firstName} {lastName}</p>
+          <small>{date}</small>
+        </div>
       </header>
     )
   }
 }
 
+class TechList extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const techs = ['HTML', 'CSS', 'JavaScript']
+    const techsFormatted = techs.map((tech) => <li key={tech}>{tech}</li>)
+    return techsFormatted
+  }
+}
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <main>
+        <div className='main-wrapper'>
+          <p>Prerequisite to get started react.js:</p>
+          <ul>
+            <TechList />
+          </ul>
+          <UserCard user={this.props.user} />
+          <Button
+            text='Show Time'
+            onClick={this.props.handleTime}
+            style={buttonStyles}
+          />
+        </div>
+      </main>
+    )
+  }
+}
+
+class Footer extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <footer>
+        <div className='footer-wrapper'>
+          <p>Copyright {this.props.date.getFullYear()}</p>
+        </div>
+      </footer>
+    )
+  }
+}
+
+class App extends React.Component {
+  showDate = (time) => {
+    const months = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+      'July',
+      'August',
+      'September',
+      'October',
+      'November',
+      'December',
+    ]
+
+    const month = months[time.getMonth()].slice(0, 3)
+    const year = time.getFullYear()
+    const date = time.getDate()
+    return ` ${month} ${date}, ${year}`
+  }
+
+  greetPeople = () => {
+    alert('Welcome to 30 Days Of React Challenge, 2020')
+  }
+
+  handleTime = () => {
+    alert(this.showDate(new Date()))
+  }
+
+  render () {
+    const data = {
+      welcome: 'Welcome to 30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 7, 2020',
+    }
+
+    const techs = ['HTML', 'CSS', 'JavaScript']
+
+    const user = {...data.author, image: personImage}
+
+    return (
+      <div className='app'>
+      <Header data={data}/>
+      <Main 
+        user={user}
+        techs={techs}
+        handleTime={this.handleTime}
+        greetPeople={this.greetPeople}
+      />
+
+      <Footer date={new Date()}/>
+    </div>
+    )
+  }
+}
+
+  
+
 const root = document.getElementById('root');
-ReactDOM.render(<Header />, root);
+ReactDOM.render(<App />, root);
