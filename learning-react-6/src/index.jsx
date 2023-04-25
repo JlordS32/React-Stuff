8e90db7f92b48b8d11030672231bfcac17c6b96f
JlordS32 <jlordsty128@gmail.com>
2023-04-26 01:09:16 +1000

    partially finished conditional statements

--- learning-react-6/src/index.jsx
+++ learning-react-6/src/index.jsx
@@ -1,13 +1,88 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Fuction to show month date year
-class App extends React.Component {
+class Header extends React.Component {
   render() {
+    const {
+      welcome,
+      title,
+      subtitle,
+      author: { firstName, lastName },
+      date,
+    } = this.props.data
+
     return (
-      <div className='container'>
-        <h1>Hello World!</h1>
+      <header>
+        <div className='header-wrapper'>
+          <h1>{welcome}</h1>
+          <h2>{title}</h2>
+          <h3>{subtitle}</h3>
+          <p>
+            {firstName} {lastName}
+          </p>
+          <small>{date}</small>
+          <p>Select a country for your next holiday</p>
+        </div>
+      </header>
+    )
+  }
+}
+
+class LogIn extends React.Component {
+  constructor (props) {
+    super(props);
+  }
+  render() {
+    const validate = this.props.validate;
+    return (
+      <div className='login-container'>
+        <h1>Sign in</h1>
+        <div className='signin-textbox'>
+          <input type='email' placeholder='Email'></input>
+          <input type='password' placeholder='Password'></input>
+        </div>
+        <button onClick={validate}>Sign in</button>
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+  state = {
+    loggedIn: false,
+  }
+  validate = () => {
+    const status = this.state.loggedIn;
+
+    const isLoggedIn = status ? false: true;
+    this.setState({ loggedIn: isLoggedIn })
+  }
+
+  render() {
+    const data = {
+      welcome: '30 Days Of React',
+      title: 'Getting Started React',
+      subtitle: 'JavaScript Library',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+      date: 'Oct 9, 2020',
+      validate: this.validate,
+    }
+
+    let status;
+
+    if (this.state.loggedIn) {
+      status = <Header data={data} />
+    }
+    else {
+      status = <LogIn validate={data.validate}/>
+    }
+
+    return (
+      <div className='app'>
+        {status}
       </div>
     )
   }
