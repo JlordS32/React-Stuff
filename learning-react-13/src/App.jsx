8bc15f2a216aaf1fe4a3b7fe561b2437dfcc409c
JlordS32 <jlordsty128@gmail.com>
2023-04-30 22:23:22 +1000

    initial update

--- learning-react-13/src/App.jsx
+++ learning-react-13/src/App.jsx
@@ -1,20 +1,139 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router } from "react-router-dom";
+import challenges from './challenges';
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+    NavLink,
+    useLocation,
+} from "react-router-dom";
 
+
+const Home = (props) => {
+    return (
+        <h1>Welcome Home! UwU</h1>
+    )
+};
+
+const About = () => <h1>About Us</h1>;
+const Contact = () => {
+    return (
+        <h1>Contact Us</h1>
+    )
+};
+
+const Challenge = ({
+    challenges: {
+      name,
+      description,
+      status,
+      days,
+      level,
+      slug,
+      url,
+      duration,
+      author: { firstName, lastName },
+    },
+  }) => (
+    <div>
+      <h1>{name}</h1>
+      <p>{level}</p>
+      <p>
+        Author: {firstName} {lastName}
+      </p>
+      {duration && (
+        <>
+          {' '}
+          <small>{duration}</small> <br />
+        </>
+      )}
+      <small>Number of days: {days}</small>
+  
+      <p>{description}</p>
+    </div>
+)
+
+const Challenges = (props) => {
+    const path = props.location.pathname + '/*';
+    const slug = path.split('/').slice(path.split('/').length -1)[0];
+    const challenge = challenges.filter(
+        (challenge) => challenge.slug === slug
+    );
+
+    return (
+        <>
+            <h1>Challenges</h1>
+            <ul>
+                {challenge.map(({name, slug}) => (
+                    <li>
+                        <NavLink to={`/challenges/${slug}`}>{name}</NavLink>
+                    </li>
+                ))}
+            </ul>
+            <Routes>
+                <Route
+                    path={'/challenges'}
+                    element={() => <h1>Choose any of the challenges</h1>}
+                />
+                <Route 
+                    path={path}
+                    element={(props) => <Challenge challenge={challenge}/>}
+                />
+            </Routes>
+        </>
+    )
+};
+
+const NotFound = (props) => <h1>The page your looking for is not found!</h1>
+
+const NavBar = () => (
+    <ul>
+        <li>
+            <NavLink to='/'>Home</NavLink>
+        </li>
+        <li>
+            <NavLink to='/about'>About</NavLink>
+        </li>
+        <li>
+            <NavLink to='/contact'>Contact</NavLink>
+        </li>
+        <li>
+            <NavLink to='/challenges'>Challenges</NavLink>
+        </li>
+    </ul>
+)
 
 class App extends Component {
-    state = {
-        title: 'Hello World!'
-    }
     render() {
         return (
-            <Router>
+            <BrowserRouter>
                 <div className='app-container'>
-                    <h1>
-                        {this.state.title}
-                    </h1>
+                    <NavBar />
+
+                    <Routes>
+                        <Route 
+                            path="/" 
+                            element={<Home />} 
+                        />
+                        <Route 
+                            path="/about" 
+                            element={<About />} 
+                        />
+                        <Route 
+                            path="/contact" 
+                            element={<Contact location={location} />}
+                        />
+                        <Route 
+                            path="/challenges/*" 
+                            element={<Challenges location={location}/>} 
+                        />
+                        <Route 
+                            path="*"
+                            element={<NotFound />}
+                        />     
+                    </Routes>
                 </div>
-            </Router>
+            </BrowserRouter>
         )
     }   
 }
---
+++ learning-react-13/src/challenges.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+
+const challenges = [
+    {
+      name: '30 Days Of Python',
+      description:
+        '30 Days of Python challenge is a step by step guide to learn Python in 30 days.',
+      status: 'completed',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '20 Nov 2019 - 20 Dec 2019',
+      slug: 'pyhton',
+      url:
+        'https://github.com/https://https://github.com/Asabeneh/30-Days-Of-Python.com/Asabeneh/30-Days-Of-JavaScript/30-Days-Of-React',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+    {
+      name: '30 Days Of JavaScript',
+      description:
+        '30 Days of JavaScript challenge is a step by step guide to learn JavaScript in 30 days.',
+      status: 'completed',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '1 Jan 2020 - 30 Jan 2020',
+      slug: 'javascript',
+      url: 'https://github.com/Asabeneh/30-Days-Of-JavaScript',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+    {
+      name: '30 Days Of React',
+      description:
+        '30 Days of React challenge is a step by step guide to learn React in 30 days.',
+      status: 'ongoing',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '1 Oct 2020- 30 Oct 2020',
+      slug: 'react',
+      url: 'https://github.com/Asabeneh/30-Days-Of-React',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+    {
+      name: '30 HTML and CSS',
+      description:
+        '30 Days of HTML and CSS challenge is a step by step guide to learn HTML and CSS in 30 days.',
+  
+      status: 'coming',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '',
+      slug: 'html-and-css',
+      url: '',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+    {
+      name: '30 ReactNative',
+      description:
+        '30 Days of ReactNative challenge is a step by step guide to learn ReactNative in 30 days.',
+      status: 'coming',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '',
+      slug: 'reactnative',
+      url: '',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+    {
+      name: '30 Data Analysis',
+      description:
+        '30 Days of Data Analysis challenge  is a step by step guide to learn about data, data visualization and data analysis in 30 days.',
+      status: 'coming',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '',
+      slug: 'data-analysis',
+      url: '',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+    {
+      name: '30 Machine Learning',
+      description:
+        '30 Days of Machine learning challenge  is a step by step guide to learn data cleaning, machine learning models and predictions in 30 days.',
+      status: 'coming',
+      days: 30,
+      level: 'Beginners to Advanced',
+      duration: '',
+      slug: 'machine-learning',
+      url: '',
+      author: {
+        firstName: 'Asabeneh',
+        lastName: 'Yetayeh',
+      },
+    },
+]
+
+export default challenges;
