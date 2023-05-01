0465bde8b06bd1eb926ded39fcccb1143a1c52d5
JlordS32 <jlordsty128@gmail.com>
2023-05-01 23:39:08 +1000

    new project

---
+++ fetchAndAxios/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+    render() {
+        <div className='app'>
+            <h1>Hi</h1>
+        </div>
+    }
+}
+
+export default App;
