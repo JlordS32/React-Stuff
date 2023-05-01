7415f07bddb86ebe4c9e91ee6bbbad32d864a9e4
JlordS32 <jlordsty128@gmail.com>
2023-05-01 23:52:52 +1000

    updates

--- fetchAndAxios/src/App.jsx
+++ fetchAndAxios/src/App.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
+import { Country } from './Country';
 
 class App extends Component {
+    state = {
+        data: []
+    }
+
+    componentDidMount() {
+        axios
+            .get('https://restcountries.com/v3.1/region/asia')
+            .then((response) => {
+                console.log(response.data);
+                this.setState({
+                    data: response.data
+                })
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    
     render() {
         <div className='app'>
-            <h1>Hi</h1>
+            <h1>React Component Life Cycle</h1>
+            <h1>Calling API</h1>
+            <div>
+                <p>There are {this.state.data.length} countries in the api</p>
+                <div className='countries-wrapper'>
+                    {this.state.data.map((country) => (
+                    <Country country={country} />
+                    ))}
+                </div>
+            </div>
         </div>
     }
 }
---
+++ fetchAndAxios/src/Country.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+export const Country = ({
+    country: {
+        name, capital, flag, language, population, currency
+    }
+}) => {
+    const formatedCapital = capital.length > 0 ? (
+        <>
+            <span>Capital: </span>
+            {capital}
+        </>
+    ) : (
+        ''
+    );
+
+    const formatLanguage = languages.length > 1 ? `Languages` : `Language`;
+    console.log(languages);
+
+    return (
+        <div className='country'>
+            <div className='country_flag'>
+                <img src={flag} alt={name} />
+            </div>
+            <h3 className='country_name'>{name.toUpperCase()}</h3>
+            <div class='country_text'>
+                <p>{formatedCapital}</p>
+                <p>
+                    <span>{formatLanguage}: </span>
+                    {languages.map((language) => language.name).join(', ')}
+                </p>
+                <p>
+                    <span>Population: </span>
+                    {population.toLocaleString()}
+                </p>
+                <p>
+                    <span>Currency: </span>
+                    {currency}
+                </p>
+            </div>
+        </div>
+    );
+};
