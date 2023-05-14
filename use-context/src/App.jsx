5943fc223901e1b33aa527feba9c136297122f61
JlordS32 <jlordsty128@gmail.com>
2023-05-14 20:58:29 +1000

    commit

---
+++ use-context/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { 
+   useState, 
+   useContext,
+   createContext,
+   useEffect } 
+from 'react';
+
+const List = ({value}) => {
+   return (
+      <li>{value}</li>
+   );
+};
+
+const TestContext = createContext(null);
+
+const App = () => {
+   const [input, setInput ] = useState(localStorage.getItem('input') || '');
+   const [lists, setLists] = useState(JSON.parse(localStorage.getItem('lists')) || []);
+
+   useEffect(() => {
+      localStorage.setItem('input', input);
+   }, [input]);
+
+   useEffect(() => {
+      localStorage.setItem('lists', JSON.stringify(lists));
+   }, [lists]);
+
+   const handleClick = () => {
+      setLists([...lists, input]);
+      setInput('');
+   };
+
+   return (
+      <div className='app-container'>
+         <input type="text" onChange={e => setInput(e.target.value)} />
+
+         <button onClick={handleClick}>
+            Add
+         </button>
+         
+         <ul>
+            {lists.map((value, index) => (
+               <List value={value} key={index}/>
+            ))}
+         </ul>
+      </div>
+   );
+};
+
+export default App;
