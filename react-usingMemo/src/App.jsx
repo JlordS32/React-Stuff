e27a4cddb31da9cceb7c801e2af35ec431f5d97c
JlordS32 <jlordsty128@gmail.com>
2023-05-23 22:09:35 +1000

    updates

--- react-usingMemo/src/App.jsx
+++ react-usingMemo/src/App.jsx
@@ -1,9 +1,58 @@
-import React from 'react';
+import React, { useState, useMemo, memo } from 'react';
+import './app.modules.css';
 
-const App = () => {
+function Swatch({color}) {
+   console.log(`Swatch rendered ${color}`);
+   return (
+      <div
+         style={{
+            margin: 2,
+            width: 75,
+            height: 75,
+            backgroundColor: color
+         }}>
+      </div>
+   )
+}
+
+const MemoedSwatch = memo(Swatch);
+
+function App() {
+   const [appRenderIndex, setAppRenderIndex] = useState(0);
+   const [colorInput, setColorInput] = useState('');
+   const [color, setColor] = useState('red');
+
+   console.log(`App rendered ${appRenderIndex}`);
+
+   const handleColorChange = (e) => {
+      setColorInput(e.target.value);
+   }
+
+   const handleClick = () => {
+      setColor(colorInput);
+   }
+
    return (
       <div className='app'>
-         <h1>App Component</h1>
+         <div
+            style={{
+               display: 'flex',
+            }}>
+            <input 
+               type='text' 
+               placeholder='Color: '
+               onChange={handleColorChange} 
+               />
+            <button onClick={handleClick}>Submit</button>
+         </div>
+         <div>
+            <button onClick={() => setAppRenderIndex(appRenderIndex + 1)}>
+               Re-Render App
+            </button>
+         </div>
+         <div>
+            <MemoedSwatch color={color} />
+         </div>
       </div>
    )
 }
