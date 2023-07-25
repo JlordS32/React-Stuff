cfc5db52ba6127c162b416051953b5b586bc121b
JlordS32 <jlordsty128@gmail.com>
2023-07-26 00:25:04 +1000

    nav is tough, just like how tought it was to get her back

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -3,13 +3,22 @@ import './style/app.modules.css';
 
 export default function App() {
 	return (
-		<div className='app-container'>
-			<div className='header-container'>This is a header</div>
-			<main>
-				<h1>About me</h1>
-				<p>Some content here, random content</p>
-			</main>
-			<footer>Jlord Style, Inc. &copy; All rights reserved.</footer>
-		</div>
+		<>
+			<div className='app-container'>
+				<div className='header-container'>This is a header</div>
+				<div className='nav-container'>
+               <div className='nav-items'>A</div>
+               <div className='nav-items'>B</div>
+               <div className='nav-items'>C</div>
+            </div>
+				<main>
+					<div className='main-content'>
+						<h1>About me</h1>
+						<p>Some content here, random content</p>
+					</div>
+				</main>
+				<footer>Jlord Style, Inc. &copy; All rights reserved.</footer>
+			</div>
+		</>
 	);
 }
