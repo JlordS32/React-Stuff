11c9bd4f5879ee51daae96d5f7137b47b94feb67
JlordS32 <jlordsty128@gmail.com>
2023-08-21 08:27:27 +1000

    i need to optimise performance

--- learning-react-router/src/components/careers/Careers.jsx
+++ learning-react-router/src/components/careers/Careers.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useMemo } from 'react';
 import styles from '../../styles/careers.module.css';
 import { useLoaderData, Link } from 'react-router-dom';
 
 function Careers() {
 	const careers = useLoaderData();
 
+	// TODO memoize/cache API Calls
+
 	return (
 		<>
-			{careers?.map((career) => {
-				return (
-					<div
-						className={styles['career-item']}
-						key={career.id}
-					>
-						<Link to={career.id.toString()}>
-							<h2>{career.title}</h2>
-							<p>Based in {career.location}</p>
-						</Link>
-					</div>
-				);
-			})}
+			{useMemo(() => {
+				return careers?.map((career) => {
+					return (
+						<div
+							className={styles['career-item']}
+							key={career.id}
+						>
+							<Link to={career.id.toString()}>
+								<h2>{career.title}</h2>
+								<p>Based in {career.location}</p>
+							</Link>
+						</div>
+					);
+				});
+			}, [careers])}
 		</>
 	);
 }
 
 export const careersLoader = async () => {
 	const res = await fetch('https://careersapi.vercel.app/api/careers');
+
+	console.log('I have been ran');
 
 	const data = await res.json();
 
